13d6994e7200c1913a2dd11730279fc3ca1db0d4
Sondos <sondosissa18@gmail.com>
2020-07-08 10:34:12 -0500

    finished the assessemtn

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { request } = require("express");
+const router = express.Router();
+const pug = require("pug");
+const User = require("./models/User");
+var bodyParser = require("body-parser");
+const app = express();
+app.set("view engine", "pug");
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/guest", async (req, res) => {
+  const GuestList = await User.find((err, rsvp) => {
+    if (err) return console.log(err);
+  });
+  console.log(GuestList);
+  res.render("Guest", {
+    attendes: GuestList.filter((rsvp) => rsvp.attending),
+    notattending: GuestList.filter((rsvp) => rsvp.attending == false),
+  });
+});
+
+app.get("/deletelist", async (req, res) => {
+  const deletelist = await User.remove();
+  res.send("deleteList");
+});
+
+app.post("/reply", (req, res) => {
+  const body = req.body;
+  console.log(req.body);
+
+  if (body === undefined) {
+    return res.status(400).json({ error: "content missing" });
+  }
+
+  const user = new User({
+    username: body.username,
+    email: body.email,
+    attending: body.attending,
+    number: body.number,
+  });
+
+  user.save().then((savedNote) => {
+    res.render("test");
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server Connected");
+});
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb://localhost:27017/users";
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  attending: Boolean,
+  number: String,
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
